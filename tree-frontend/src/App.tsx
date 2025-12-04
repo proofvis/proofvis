f76794ac6289dc5dc0f751a776a7fc6c986c3927
James Eschrich <jaeschrich@gmail.com>
2025-12-04 11:28:06 -0600

    put basic JSON basic in the App (and switched a new branch on the same repo)

--- tree-frontend/src/App.tsx
+++ tree-frontend/src/App.tsx
@@ -4,30 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const tree = {
+    "proof_state": {"hypotheses": [], "goal": "P \/ Q -> Q \/ P"},
+    "next_tactic": "intros H",
+    "children": [
+      {
+        "proof_state": { "hypotheses": ["P \/ Q"], "goal": "Q \/ P" },
+        "next_tactic": "cases H",
+        "children": [
+          {
+            "proof_state": { "hypotheses": ["P"], "goal": "Q \/ P" },
+            "next_tactic": "right",
+            "children": []
+          },
+          {
+            "proof_state": { "hypotheses": ["Q"], "goal": "Q \/ P" },
+            "next_tactic": "left",
+            "children": []
+          }
+        ]
+      }
+    ]
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>This will be a tree-view!</p>
     </>
   )
 }
