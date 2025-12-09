dfa1941dc7f7792ea7df518ee97daa19ee2c74f2
Kevin Fisher <kfish610@gmail.com>
2025-12-09 12:17:35 -0600

    (unsucceful) attempts at including the vite project widget

--- Proofvis/Basic.lean
+++ Proofvis/Basic.lean
@@ -1,9 +1,28 @@
 import Lean
 
-open Lean Widget
+open Lean Widget Elab Command
 
 @[widget_module]
 def helloWidget : Widget.Module where
-  javascript := include_str ".." / "widget" / "src" / "basic.js"
+  javascript := include_str ".." / "tree-frontend" / "dist" /"build.js"
+
+-- #html <Tree aksfdj="dkafja"></Tree>
 
 #widget helloWidget
+
+-- #extract hello
+#check True
+
+def extract (name : Ident) : CommandElabM String := do
+  return include_str ".." / "temp.json"
+
+elab "#visualize" name:ident : command => do
+  liftTermElabM do
+    let wi : Expr ← elabWidgetInstanceSpec `(helloWidget)
+    let wi : WidgetInstance ← evalWidgetInstance wi
+    savePanelWidgetInfo wi.javascriptHash wi.props stx
+
+theorem hello (n : Nat) : n = n := by rfl
+
+
+#visualize hello
